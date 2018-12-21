95fe07ca0d45a792dff6e2a873f380c25e976a39
Mopip77 <mopip77@qq.com>
2018-12-21 20:59:04 +0800

    修复了声明变量出现赋值语句后不可继续声明的问题, 但是修改了部分文法,弱化了赋值语句的使用范围

--- v.cpp
+++ v.cpp
@@ -1,11 +1,12 @@
 int x[2][13];
+int y = 1, m, z = m + y,c;
 
 int func(int y){
     int a=x[1][5];
     return a;
 }
 int main(){
-    int x = x +1;
+    int x = x +1,y;
 	//x[7][7] = 1;  //你就是个呆瓜
     //int //;//x = 3;
 }
